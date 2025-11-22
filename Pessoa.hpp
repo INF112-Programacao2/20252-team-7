1ac6098e9e794231c792f980ca376a0906fa6c1e
luisrrsouza-png <luis.r.souza@ufv.br>
2025-11-22 11:08:18 -0300

    Luís

--- Pessoa.hpp
+++ Pessoa.hpp
@@ -8,7 +8,7 @@ class Pessoa {
     private:
         std::string _nome;
         std::string _endereco;
-        const int _cpf;
+        std::string _cpf; // luis: int para string
         Material _material;
     public:
         Pessoa( std::string nome, std::string endereco, int cpf, Material material ); //Construtor
@@ -17,17 +17,16 @@ class Pessoa {
         //Funções get
         std::string getNome( );
         std::string getEndereco( );
-        int getCpf( );
+        std::string getCpf( ); //luis: int para string
         Material getMaterial( );
 
         //Funções set
-        void setNome( std::string nome );
-        void setEndereco( std::string endereco );
-        void setMaterial( Material material );
+        void setNome(std::string nome);
+	    void setEndereco(std::string endereco);
+	    void setMaterial(Material material);
 
-        //Métodos
-		virtual void definirMaterial(Material, float, int);
-        void cadastro(std::string, std::string, int, Material);
-};
+		//Métodos
+	    void definirMaterial(Material mat, float p, int t);
+	    void cadastro(std::string nome, std::string endereco, std::string cpf, Material material);
 
-#endif;
+#endif //OBS: após o endif, NÃO UTILIZA-SE ";"
