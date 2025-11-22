eaa644303c1574ce04b3a707c8990d65362628cf
luisrrsouza-png <luis.r.souza@ufv.br>
2025-11-22 17:23:59 -0300

    Refactor Pessoa class to use pointers for Material

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -1,55 +1,60 @@
 #include "Pessoa.hpp"
-#include "material.hpp"
-#include <string>
+#include "material.hpp"  // AGORA pode incluir aqui
+#include <iostream>
 #include <fstream>
 
-//Construtor
-Pessoa::Pessoa( std::string nome, std::string endereco, int cpf, Material material ) :
-    _nome( nome ), _endereco( endereco ), _cpf( cpf ), _material( material ) { };
+// Construtor
+Pessoa::Pessoa(std::string nome, std::string endereco, std::string cpf, Material* material) :
+    _nome(nome), _endereco(endereco), _cpf(cpf), _material(material) { }
 
-Pessoa::~Pessoa( ) { };
+Pessoa::~Pessoa() { }
 
-//Funções get
-std::string Pessoa::getNome( ) {
-    return this->_nome;
-}
-std::string Pessoa::getEndereco( ) {
-    return this->_endereco;
-}
-std::string Pessoa::getCpf( ) {
-    return this->_cpf;
-}
-Material Pessoa::getMaterial( ) {
-    return this->_material;
+// Getters
+std::string Pessoa::getNome() {
+    return _nome;
 }
 
-//Funções set
-void Pessoa::setNome( std::string novoNome ) {
-    this->_nome = novoNome;
-}
-void Pessoa::setEndereco( std::string novoEndereco ) {
-    this->_endereco = novoEndereco;
-}
-void Pessoa::setMaterial( Material novoMaterial ) {
-    this->_material = novoMaterial;
+std::string Pessoa::getEndereco() {
+    return _endereco;
 }
 
-//Métodos
-virtual void Pessoa::definirMaterial(Material mat, float p, int t) {
-    mat.setPeso(p);
-    mat.setTipo(t);
+std::string Pessoa::getCpf() {
+    return _cpf;
 }
 
-void Pessoa::cadastro(std::string nome, std::string endereco, std::string cpf, Material material) {
-	std::ofstream arquivo("cadastro.txt", std::ios::app);
+Material* Pessoa::getMaterial() {
+    return _material;
+}
 
+// Setters
+void Pessoa::setNome(std::string novoNome) {
+    _nome = novoNome;
+}
+
+void Pessoa::setEndereco(std::string novoEndereco) {
+    _endereco = novoEndereco;
+}
+
+void Pessoa::setMaterial(Material* novoMaterial) {
+    _material = novoMaterial;
+}
+
+// Métodos
+void Pessoa::definirMaterial(Material* mat, float p, int t) {
+    mat->setPeso(p);
+    mat->setTipo(t);
+}
+
+void Pessoa::cadastro(std::string nome, std::string endereco, std::string cpf, Material* material) {
+    std::ofstream arquivo("cadastro.txt", std::ios::app);
+    
     if (!arquivo.is_open()) {
-		std::cerr << "Erro ao abrir o arquivo de cadastro." << std::endl;
-        return; //obs:função void não retorna "1"
+        std::cerr << "Erro ao abrir o arquivo de cadastro." << std::endl;
+        return;
     }
-
+    
     arquivo << nome << "," << endereco << "," << cpf << "," 
-            << material.getPeso() << "," << material.getTipo();
-
-	arquivo.close();
+            << material->getPeso() << "," << material->getTipo() << std::endl;
+    
+    arquivo.close();
 }
